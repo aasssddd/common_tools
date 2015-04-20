803098d5da6bfe1dd6c0c537e374aafdd6ef8068
unknown <828_WilliamChen@OR030401.symphox.com>
2015-04-20 13:03:11 +0800

    merged branch iptables

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,8 +11,3 @@ include_recipe 'mlocate'
 include_recipe 'yum-epel'
 include_recipe 'yum-elrepo'
 include_recipe 'iftop'
-include_recipe 'iptables'
-
-iptables_rule 'http' do 
-	action :enable
-end
