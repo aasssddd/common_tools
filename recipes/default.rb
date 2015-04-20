e56b46c9de09cfed300220bd73f68bfdcea94387
unknown <828_WilliamChen@OR030401.symphox.com>
2015-04-20 11:39:25 +0800

    add iptables cookbook, default open http 80 port

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,3 +11,8 @@ include_recipe 'mlocate'
 include_recipe 'yum-epel'
 include_recipe 'yum-elrepo'
 include_recipe 'iftop'
+include_recipe 'iptables'
+
+iptables_rule 'http' do 
+	action: enable
+end
